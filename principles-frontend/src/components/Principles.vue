570ef62d99e7a09c40416c22d11cd4901e05d281
austinkelsay <austinkelsay@yahoo.com>
2020-11-12 12:44:03 -0600

    Got Principles rendering with vuetify card

--- principles-frontend/src/components/Principles.vue
+++ principles-frontend/src/components/Principles.vue
@@ -1,5 +1,42 @@
 <template>
-    <div v-on:welcome="getPrinciples">
+    <div>
+        <v-card v-bind:key='principle.id' v-for="principle in principles" :loading="loading" class="mx-auto my-12" max-width="58%">
+            <template slot="progress">
+                <v-progress-linear
+                    color="deep-purple"
+                    height="10"
+                    indeterminate
+                ></v-progress-linear>
+            </template>
+            <v-card-title>Problem:</v-card-title>
+            <v-card-text>
+                <div class="my-1 subtitle-1">
+                    {{principle.problem}}
+                </div>
+                <br/>
+                <div color='black' class="my-4 subtitle-1">
+                    Diagnosis:
+                </div>
+                <div class="my-4 subtitle-1">
+                    {{principle.diagnosis}}
+                </div>
+                <div color='black' class="my-4 subtitle-1">
+                    Change:
+                </div>
+                <div color='black' class="my-4 subtitle-1">
+                    {{principle.change}}
+                </div>
+            </v-card-text>
+            <v-card-actions>
+                <v-btn
+                    color="deep-purple lighten-2"
+                    text
+                    @click="reserve"
+                >
+                    Adopt Principle
+                </v-btn>
+            </v-card-actions>
+        </v-card>
     </div>
 </template>
 
@@ -7,6 +44,11 @@
 import axios from 'axios'
 export default {
     name: 'Principles',
+    data: () => ({
+        principles: [],
+        loading: false,
+        selection: 1,
+    }),
     created: function() {
         axios.get('https://principles-backend.herokuapp.com/principles', {
                 headers: {
@@ -14,13 +56,18 @@ export default {
                 }
             })
             .then((res) => {
-                console.log(res)
+                this.principles = res.data
             })
             .catch((err) => {
                 console.log(err)
             })
     },
     methods: {
+        reserve () {
+        this.loading = true
+
+        setTimeout(() => (this.loading = false), 2000)
+      },
     }
 }
 </script>
