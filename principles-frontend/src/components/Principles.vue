8082cce3f85ed1a87f89cd5181caaca952b313bc
austinkelsay <austinkelsay@yahoo.com>
2020-11-13 14:17:08 -0600

    Styled principle cards

--- principles-frontend/src/components/Principles.vue
+++ principles-frontend/src/components/Principles.vue
@@ -1,35 +1,24 @@
 <template>
     <div>
-        <v-card v-bind:key='principle.id' v-for="principle in principles" :loading="loading" class="mx-auto my-12" max-width="58%">
-            <template slot="progress">
-                <v-progress-linear
-                    color="deep-purple"
-                    height="10"
-                    indeterminate
-                ></v-progress-linear>
-            </template>
+        <v-card dark v-bind:key='principle.id' v-for="principle in principles"  class="mx-auto my-12" max-width="55%">
             <v-card-title>Problem:</v-card-title>
             <v-card-text>
-                <div class="my-1 subtitle-1">
+                <div class="subtitle-1 highlight">
                     {{principle.problem}}
                 </div>
                 <br/>
-                <div color='black' class="my-4 subtitle-1">
-                    Diagnosis:
-                </div>
+                <v-card-title class="inner-title">Diagnosis:</v-card-title>
                 <div class="my-4 subtitle-1">
                     {{principle.diagnosis}}
                 </div>
-                <div color='black' class="my-4 subtitle-1">
-                    Change:
-                </div>
-                <div color='black' class="my-4 subtitle-1">
+                <v-card-title class="inner-title">Change:</v-card-title>
+                <div class="my-4 subtitle-1">
                     {{principle.change}}
                 </div>
             </v-card-text>
             <v-card-actions>
                 <v-btn
-                    color="deep-purple lighten-2"
+                    color="#D70913"
                     text
                     @click="reserve"
                 >
@@ -73,5 +62,16 @@ export default {
 </script>
 
 <style>
-
+.highlight {
+    width: 60%;
+    padding: 1%;
+    font-size: 1.2rem;
+    text-align: center;
+    background-color: #d70913d7;
+    border-radius: 5px;
+}
+.inner-title {
+    padding: 0%;
+    color: white;
+}
 </style>
